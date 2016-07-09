c7e1276eb9cee2c257cc96f304b34225b4d8848e
Volt-Ran <satta-alina@yandex.ru>
2016-07-09 10:37:08 +0500

    h4

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,8 +1,14 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.graphics.BitmapFactory;
+import android.provider.Settings;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -21,28 +27,71 @@ import com.softdesign.devintensive.R;
 import android.support.design.widget.NavigationView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.RelativeLayout;
+
+
+
+import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AlertDialog;
+import android.widget.RelativeLayout;
 
 
 import com.softdesign.devintensive.utils.ConstantManager;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
 
 
     private CoordinatorLayout mCoordinatorLayout;
-    private  Toolbar mToolbar;
+    private Toolbar mToolbar;
     private DrawerLayout mNavigationDrawer;
     private ImageView mImageView;
 
     protected EditText mEditText;
     protected String mColorMode;
+    private RelativeLayout mProfilePlaceholder;
+
+
+    mProfilePlaceholder=(RelativeLayout):
+
+    findViewById(R.id.profile_placeholder);
+
+    mProfilePlaceholder.setOnClickListener(this);
+    mProfilePlaceholder = (RelativeLayout) findViewById(R.id.profile_placeholder);
+    mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+    mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+    mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    mFab = (FloatingActionButton) findViewById(R.id.fab);
+    mAppBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+    mMainProfileImage = (ImageView) findViewById(R.id.main_profile_image);
+    mCall = (ImageView) findViewById(R.id.button_call);
+    mCall.setOnClickListener(this);
+    mSendEmail = (ImageView) findViewById(R.id.button_send_email);
+    mSendEmail.setOnClickListener(this);
+    mShowVk = (ImageView) findViewById(R.id.button_open_vk);
+    mShowVk.setOnClickListener(this);
+    mShowRepos = (ImageView) findViewById(R.id.button_open_repos);
+    mShowRepos.setOnClickListener(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
@@ -88,6 +137,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         Log.d(TAG, "onDestroy");
     }
 
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -100,28 +150,29 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
 
 
+    private void setupDrawerHeader(NavigationView parent, Bitmap avatar, String name, String email) {
+        View view = parent.getHeaderView(0);
+        if (avatar != null) {
+            RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(getResources(), avatar);
+            dr.setCircular(true);
+            ImageView imageView = (ImageView) view.findViewById(R.id.avatar);
+            imageView.setImageDrawable(dr);
+        }
+        if (name != null) {
+            TextView textView = (TextView) view.findViewById(R.id.user_name_txt);
+            textView.setText(name);
+        }
+        if (email != null) {
+            TextView textView = (TextView) view.findViewById(R.id.user_email_txt);
+            textView.setText(email);
+        }
+    }
 
-    private void setupDrawerHeader(NavigationView parent, Bitmap avatar, String name, String email){
-                View view = parent.getHeaderView(0);
-                if (avatar != null) {
-                        RoundedBitmapDrawable dr= RoundedBitmapDrawableFactory.create(getResources(),avatar);
-                        dr.setCircular(true);
-                        ImageView imageView = (ImageView)view.findViewById(R.id.avatar);
-                        imageView.setImageDrawable(dr);
-                    }
-                if (name != null){
-                       TextView textView = (TextView)view.findViewById(R.id.user_name_txt);
-                       textView.setText(name);
-                   }
-                if (email != null){
-                        TextView textView = (TextView)view.findViewById(R.id.user_email_txt);
-                        textView.setText(email);
-                    }
-            }
-
-   
 
     private void setupToolbar() {
         setSupportActionBar(mToolbar);
@@ -134,10 +185,86 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         }
     }
 
+    showProfilePlaceholder();
 
+
+    private void hideProfilePlaceholder() {
+        mProfilePlaceholder.setVisibility(View.GONE);
+    }
+
+    private void showProfilePlaceholder() {
+                mProfilePlaceholder.setVisibility(View.VISIBLE);
+            }
+}
+
+    private void lockToolbar() {
+        mAppBarLayout.setExpanded(true, true);
+        mAppBarParams.setScrollFlags(0);
+        mCollapsingToolbar.setLayoutParams(mAppBarParams);
+    }
+
+    private void unlockToolbar() {
+        mAppBarParams.setScrollFlags(
+                AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL |
+                        AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED
+        );
+        mCollapsingToolbar.setLayoutParams(mAppBarParams);
+    }
 
     @Override
-    public void onClick(View view) {
-        Log.d(TAG, "onClick");
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case ConstantManager.LOAD_PROFILE_PHOTO:
+                String[] selectItems = {
+                        getString(R.string.user_profile_dialog_gallery),
+                        getString(R.string.user_profile_dialog_camera),
+                        getString(R.string.user_profile_dialog_cancel)
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.user_profile_dialog_title));
+                builder.setItems(selectItems, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int choiceItem) {
+                        switch (choiceItem) {
+                            case 0:
+                                loadPhotoFromGallery();
+                                break;
+                            case 1:
+                                loadPhotoFromCamera();
+                                break;
+                            case 3:
+                                dialog.cancel();
+                                break;
+                        }
+                    }
+                });
+                return builder.create();
+
+            default:
+                return null;
+        }
+    }
+
+    private void insertProfileImage(Uri selectedImage) {
+        showSnackBar("insertImage" + selectedImage);
+        Picasso.with(this)
+                .load(selectedImage)
+                // TODO: 01.07.16 сделать placeholder, transform + crop
+                .into(mProfileImage);
+
+        mDataManager.getPreferencesManager().saveUserPhoto(selectedImage);
+    }
+
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(appSettingsIntent, ConstantManager.PERMISSION_SETTINGS_REQUEST_CODE);
     }
 }
+
+
+
+
+
+
